0a7b2158a6a5b8bed089cbb6ae07a12cec291a41
Ben Hubbard <plusplusben@gmail.com>
2019-01-20 20:50:39 -0500

    make it a file

--- migrate.js
+++ migrate.js
@@ -4,6 +4,8 @@ var knex = require("knex")({
 	client: "pg",
 	connection: process.env.DATABASE_URL,
 });
+var fs = require('fs');
+var accounts = JSON.parse(fs.readFileSync("./public/accounts.json", "utf8"));
 
 knex.schema.hasTable("accounts").then(function(exists) {
 	if (!exists) {
@@ -12,13 +14,7 @@ knex.schema.hasTable("accounts").then(function(exists) {
 			t.string("name", 100);
 			t.text("description");
 		}).then(() => {
-			return knex("accounts").insert([{
-				name: "Account 1",
-				description: "Description 1",
-			},{
-				name: "Account 2",
-				description: "Description 2",
-			}]);
+			return knex("accounts").insert(accounts);
 		});
 	}
 }).then(() => {
